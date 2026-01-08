ef5206dde287424b2f48e4fb0a3cada1b454d740
Jovanka <samudrajovanka20@gmail.com>
2026-01-08 23:04:31 +0700

    fix: update initial delay time after preload to 0.5

--- src/config/app.ts
+++ src/config/app.ts
@@ -4,7 +4,7 @@ const app = {
 		"Hello, I'm Jovanka Samudra. Let's connect and collaborate on building something amazing together!",
 	keywords: ['jovan', 'samudra', 'jovanka', 'samudrajovanka'],
 	url: process.env.APP_URL ?? 'http://localhost:3000',
-	initialDelayTimeAfterPreload: 0.3,
+	initialDelayTimeAfterPreload: 0.5,
 };
 
 export default app;
